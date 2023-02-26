7a68df9ee5fe49fa5a137a281f49437200db6630
huutanphp <83548070+huutanphp@users.noreply.github.com>
2023-02-26 17:59:11 +0700

    Update Anti-Adblock-Detect.js

--- Anti-Adblock-Detect.js
+++ Anti-Adblock-Detect.js
@@ -1,13 +1,15 @@
 // ==UserScript==
-// @name     _delete Adblock blocking nodes
-// @match    *://traffic1s.com/*
+// @name         Anti Adblock Detect
+// @namespace    github.com
+// @version      1.2
+// @description  Unlock all Fireship PRO courses/lessons.
+// @author       huutan
+// @match        https://traffic1s.com/*
+// @icon         https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/325/fire_1f525.png
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @grant    GM_addStyle
-// @grant    GM.getValue
+// @grant        none
 // ==/UserScript==
-//- The @grant directives are needed to restore the proper sandbox.
-/* global $, waitForKeyElements */
 
 waitForKeyElements ("[id$='ignielAdBlock']", killNode);
 
